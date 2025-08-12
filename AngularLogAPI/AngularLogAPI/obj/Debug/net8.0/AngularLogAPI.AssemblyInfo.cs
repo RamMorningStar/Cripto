d63cac30f58dc6a9ae88de5923bde41a81da3217
Ram <spiderxtrem1@hotmail.com>
2025-08-12 13:10:22 -0600

    Migracion a postgresql en railwat, eliminado EnsureCreated

--- AngularLogAPI/AngularLogAPI/obj/Debug/net8.0/AngularLogAPI.AssemblyInfo.cs
+++ AngularLogAPI/AngularLogAPI/obj/Debug/net8.0/AngularLogAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AngularLogAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d70127d876138423f7224ff1cbe1b213ae43ed69")]
 [assembly: System.Reflection.AssemblyProductAttribute("AngularLogAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AngularLogAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
